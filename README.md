5109e853f3ee05e6b149157bcf621040d40d7b8c
Rasmus R. Paulsen <rapa@dtu.dk>
2021-01-11 13:52:51 +0100

    updates

--- README.md
+++ README.md
@@ -4,4 +4,12 @@ Tools for diagnosing and sorting series of DICOM files
 ## Requirements
 - Python 3.6 (it does now work with Python 3.7 or higher due to GDCM)
 - GDCM (https://anaconda.org/conda-forge/gdcm)
+- pydicom
+
+## dicom_sorter
+
+The dicom_sorter.py script can sort DICOM files into a hiearchical sub-structure. It starts by (for each file) extracting relevant DICOM header information as patient id
+, study dates and times. These values can then later be used to define the sub-structure of the output files. 
+
+This script is mostly a copy of the code described by  Alex Weston (https://towardsdatascience.com/a-python-script-to-sort-dicom-files-f1623a7f40b8)
 
--- dicom_sorter.py
+++ dicom_sorter.py
@@ -1,5 +1,5 @@
-# Alex Weston
-# Digital Innovation Lab, Mayo Clinic
+# Rasmus R. Paulsen, DTU Compute
+# Code examples from Alex Weston, Digital Innovation Lab, Mayo Clinic
 
 import os
 import pydicom  # pydicom is using the gdcm package for decompression
@@ -13,42 +13,32 @@ def clean_text(string):
     return string.lower()
 
 
-# user specified parameters
-# src = "D:/Data/IMM/Kristine/RHTestJan2021/SE00001/"
-# dst = "D:/Data/IMM/Kristine/RHTestJan2021/SE00001/out/"
+# source DICOM directory
+src = "D:/Data/IMM/Kristine/RHTestJan2021/SE00001/"
 
-#src = "D:/Data/IMM/Kristine/ODB_June2020/0305473111/Ct Hjerte Uden Og Med Kontrast/HALF 312ms 1.20s Cardiac 0.5 CTA-HALF CE - 6"
-#dst = "D:/Data/IMM/Kristine/ODB_June2020/0305473111/Ct Hjerte Uden Og Med Kontrast/HALF 312ms 1.20s Cardiac 0.5 CTA-HALF CE - 6/out"
-
-src = "D:/Data/IMM/Kristine/ODB_June2020/1902470553/1902470553/Ct Hjerte Med Kontrast/CCTA function + venøs smartphase - 303"
-dst = "D:/Data/IMM/Kristine/ODB_June2020/1902470553/1902470553/Ct Hjerte Med Kontrast/CCTA function + venøs smartphase - 303/out2"
-
+# where to put the results
+dst = "D:/Data/IMM/Kristine/RHTestJan2021/SE00001_sorted/"
 
 print('reading file list...')
 unsortedList = []
 for root, dirs, files in os.walk(src):
     for file in files:
+        # the next line can be used if you need to filter files
         # if ".dcm" in file:  # exclude non-dicoms, good for messy folders
         unsortedList.append(os.path.join(root, file))
 
 print('%s files found.' % len(unsortedList))
 
-# study_UID_old = ""
-# serie_UID_Old = ""
-
 for dicom_loc in unsortedList:
-    # read the file
     ds = pydicom.read_file(dicom_loc, force=True)
 
-    # get patient, study, and series information
+    # get information from DICOM header
     patientID = clean_text(ds.get("PatientID", "NA"))
     patient_name = ds.get("PatientName", "NA")
     real_patient_name = clean_text(patient_name.family_name)
     studyDate = clean_text(ds.get("StudyDate", "NA"))
     studyDescription = clean_text(ds.get("StudyDescription", "NA"))
     seriesDescription = clean_text(ds.get("SeriesDescription", "NA"))
-
-    # generate new, standardized file name
     modality = ds.get("Modality", "NA")
     studyInstanceUID = ds.get("StudyInstanceUID", "NA")
     seriesInstanceUID = ds.get("SeriesInstanceUID", "NA")
@@ -63,30 +53,15 @@ for dicom_loc in unsortedList:
     in_stack = str(ds.get("InStackPositionNumber", "0"))
     stack_id = str(ds.get("StackID", "0"))
     patient_birth = str(ds.get("PatientBirthDate", "0"))
-    # fileName = modality + "." + seriesInstanceUID + "." + instanceNumber + ".dcm"
-    #fileName = "acq." + acquisition_time + ".inst." + instanceNumber + ".inStack." + in_stack + ".stackID." + \
-    #          stack_id + ".contTime." + content_time + ".procTime." + proc_time + \
-    #          ".deltaTime."  + ".dcm"
-    # im_number = int(instanceNumber)
-    # fileName = f"IM{im_number:{5}}.dcm"
 
-    # print("StudyDesc:", studyDescription, "SeriesDesc:", seriesDescription, "Instance#", instanceNumber,
-    #      "Acq.#", acquisition_number, "acqTime", acquisition_time, "procTime", proc_time,
-    #      "contTime", content_time)
-
+    # some debug printing
     print("Instance#", instanceNumber,
           "Acq.#", acquisition_number, "acqTime", acquisition_time, "procTime", proc_time,
           "contTime", content_time, "seriesTime", series_time)
 
+    # Create a simple file name that is just "IM" + the number of the DICOM file
     fileName = "IM" + instanceNumber.zfill(5) + ".dcm"
-
-    # if study_UID_old != studyInstanceUID:
-    #     print("new study UID")
-    # if serie_UID_Old != seriesInstanceUID:
-    #    print("new study UID")
-    # study_UID_old = studyInstanceUID
-    # serie_UID_Old = seriesInstanceUID
-
+    print(fileName)
 
     # uncompress files (using the gdcm package)
     try:
@@ -96,9 +71,9 @@ for dicom_loc in unsortedList:
         # patientID, studyDate, studyDescription, seriesDescription))
         print('an instance in file %s" could not be decompressed. exiting.' % (fileName))
 
-    print(fileName)
+    # Here you can define the path structure of the output - it should depend on the information in
+    # your DICOM series
 
-    # save files to a 4-tier nested folder structure
     #if not os.path.exists(os.path.join(dst, real_patient_name)):
     #    os.makedirs(os.path.join(dst, real_patient_name))
 
@@ -108,17 +83,14 @@ for dicom_loc in unsortedList:
     # if not os.path.exists(os.path.join(dst, patientID, studyDate, studyDescription)):
     #    os.makedirs(os.path.join(dst, patientID, studyDate, studyDescription))
 
-    # if not os.path.exists(os.path.join(dst, real_patient_name, studyDate, content_time)):
-    #     os.makedirs(os.path.join(dst, real_patient_name, studyDate, content_time))
-    # ds.save_as(os.path.join(dst, real_patient_name, studyDate, content_time, fileName))
+    # real_patient_name - study date - content time
+    # works for some series and not for others
+    if not os.path.exists(os.path.join(dst, real_patient_name, studyDate, content_time)):
+         os.makedirs(os.path.join(dst, real_patient_name, studyDate, content_time))
+    ds.save_as(os.path.join(dst, real_patient_name, studyDate, content_time, fileName))
 
-    if not os.path.exists(os.path.join(dst, real_patient_name, studyDate, acquisition_time)):
-        os.makedirs(os.path.join(dst, real_patient_name, studyDate, acquisition_time))
-    ds.save_as(os.path.join(dst, real_patient_name, studyDate, acquisition_time, fileName))
-
-
-    #   print('Saving out file: %s - %s - %s - %s.' % (patientID, studyDate, studyDescription, seriesDescription))
-
-    # ds.save_as(os.path.join(dst, patientID, studyDate, studyDescription, seriesDescription, fileName))
+    # if not os.path.exists(os.path.join(dst, real_patient_name, studyDate, acquisition_time)):
+    #    os.makedirs(os.path.join(dst, real_patient_name, studyDate, acquisition_time))
+    # ds.save_as(os.path.join(dst, real_patient_name, studyDate, acquisition_time, fileName))
 
 print('done.')
